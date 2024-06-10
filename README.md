a496bb7340eaa8fe40e68ddbfd41d5bf69adc2ff
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-06-10 17:19:37 +0100

    Code the logic of the game

--- README.md
+++ README.md
@@ -11,40 +11,51 @@ Hangman is a classic game in which a player thinks of a word and the other playe
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
 ### What does Hangman do?
-Hangman selects a raondom word form a list.
+Hangman selects a random word form a list.
 It asks the user to guess a letter.
 If the input is not one letter of the alphabet, the user is prompted to enter a single alphabetical character.
 If the input has already been guessed, the user is prompted to try a different letter.
-It checks if the letter is in the word. If the guess is correct, the user is informed, and the blank word and number of characters to guess are updated. If the guess is incorrect, the number oflives is reduced and the user is informed. Regardless of whether the guess is correct or not, it is logged so that repeat guesses can be checked.
+It checks if the letter is in the word. If the guess is correct, the user is informed, and the blank word and number of characters to guess are updated. If the guess is incorrect, the number of lives is reduced and the user is informed. Regardless of whether the guess is correct or not, it is logged so that repeat guesses can be checked.
+If the user guesses all the letters correctly before they run out of lives, they win. If the run out of lives before they can guess all the letters correctly, they lose.
 
 
 ### What was the aim?
+The aim of the porject was to demonstrate the skills learned to date on the AICore course.
 
 ### What did I learn?
+I learned how to write and debug classes to create an interactive command line game. I documented my code and created this README file.
 
 ## Installation Instructions
 ### Necessary software
-- conda
 - git
 - VSCode (or other IDE)
 
 
 ### Environment setup
 1. Clone the repository
-2. Create the conda environment using the file provided
 
 ### Installing dependancies
+This project does not require any dependancies. 
 
-### Final steps
 
 ## Usage Instructions
 In the command line type:
 ```
-python3 milestone_3.py
+python3 milestone_5.py
 ```
 When prompted, type a single letter you think is in the word. 
 
 
 ## File Structure
+* [hangman](./hangman/)
+    * [hangman_Template.py](./hangman/hangman_Template.py)
+* [milestones](./milestones/)
+    * [milestone_2.py](./milestones/milestone_2.py)
+    * [milestone_3.py](./milestones/milestone_3.py)
+    * [milestone_4.py](./milestones/milestone_4.py)
+    * [milestone_5.py](./milestones/milestone_5.py)
+* [README.md](README.md)
+
 
 ## License Information
+GNU General Public License (GPL) v3.0
---
+++ milestones/milestone_5.py
@@ -0,0 +1,124 @@
+import random
+
+class Hangman:
+    """
+    This class is used to represent a game of hangman.
+
+    Attributes:
+        word (str): the word to be guessed
+        word_guessed( list): a list of blanks and letters guessed so far
+        num_letters (int): the number of unique letters left to guess
+        num_lives (int): the number of guesses remaining
+        word_list (list): the words given by the user which the computer can select from
+        list_of_guesses (list): the letters already guessed by the user
+    """
+    def __init__(self, word_list, num_lives=5) -> None:
+        """
+        See help(Hangman) for accurate signature
+        """
+        self.word = random.choice(word_list)
+        self.word_guessed:list = ['_' for letter in list(self.word)]
+        self.num_letters: int = len(set(self.word) - set(self.word_guessed))
+        self.num_lives: int = num_lives
+        self.word_list: list = word_list
+        self.list_of_guesses: list = []
+    
+
+    def ask_for_input(self):
+        """
+        This function prompts the user to input a single letter and responds to that input.
+        """
+        guess = input("Enter a single letter.")
+        self.__respond_to_input(guess)
+
+
+    def __respond_to_input(self, guess):
+        """
+        This function decides how to respond to the user's input.
+
+        If the guess is not a single alphabetical character, the user 
+        is informed and prompted to try again. If the input has been 
+        guessed previously, the user is informaed and prompted to guess 
+        again. Otherwise, the guess is checked and added to the list 
+        of guesses.
+
+        Args:
+            guess (str): The user's input when instructed to enter a single letter.
+        """
+        if not(len(guess) == 1 & guess.isalpha()):
+            print("Invalid letter. Please, enter a single alphabetical character.")
+        elif guess in self.list_of_guesses:
+            print("You already tried that")
+        else:
+            self.__check_guess(guess)
+            self.list_of_guesses.append(guess)
+
+    
+    def __check_guess(self, guess: str) -> None:
+        """
+        This function decides whether the guess is in the word or not.
+
+        The guess is converted to lowercase, then, if the guess is in the word, the user is informed and the game 
+        status is updated. If the guess is not in the word, the number 
+        of lives are reduced and the user is informed.
+
+        Args:
+            guess (str): a single alphabetical character
+        """
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            self.__reveal_letter_guessed(guess)
+            self.__update_number_of_letters_left_to_guess()
+        else:
+            self.__reduce_number_of_lives()
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+
+    def __reveal_letter_guessed(self, guess):
+        """
+        This function reveals where the guess is present in the word.
+
+        The guessed letter is placed into the blank space in word_guessed.
+
+        Args:
+            guess (str): a single alphabetical character
+        """
+        for i, letter in enumerate(self.word):
+            if letter == guess:
+                self.word_guessed[i] = guess
+
+
+    def __update_number_of_letters_left_to_guess(self):
+        """
+        This function reduces the number of letters left to guess by one.
+        """
+        self.num_letters = self.num_letters - 1
+
+    
+    
+    def __reduce_number_of_lives(self):
+        """
+        This function reduces the number of lives left by one.
+        """
+        self.num_lives = self.num_lives -1
+    
+
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+    while True:
+        if game.num_lives == 0:
+            print("You lost!")
+            break
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print("Congratulations. You won the game!")
+            break
+
+
+play_game(["mango", "grape", "pear", "peach", "banana"])
+
+            
